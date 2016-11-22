578efaedeeceedc2d915443e5ef23622b8fc7792
cogmob <luke.avery@live.co.uk>
2016-11-22 16:16:36 +0000

    fixed arguments being used from 0,1 to 2,3

--- src/lambda_pattern/lambda_pattern.es6
+++ src/lambda_pattern/lambda_pattern.es6
@@ -3,7 +3,7 @@ const store = require('./store');
 const path = require('path');
 
 const lambda_pattern = (cb) => {
-    const command = process.argv[0];
+    const command = process.argv[2];
 
     if (command === 'create') {
         return create_fn(cb);}
@@ -14,13 +14,13 @@ const lambda_pattern = (cb) => {
     cb('command \'' + command + '\' not found ... help text ...');};
 
 const create_fn = (cb) => {
-    if (!process.argv[1]) {
+    if (!process.argv[3]) {
         return cb('no project name given');}
-    create(process.cwd(), process.argv[1], cb);};
+    create(process.cwd(), process.argv[3], cb);};
 
 const store_fn = (cb) => {
-    if (!process.argv[1]) {
+    if (!process.argv[3]) {
         return cb('please specify either bitbucket or github');}
-    store(process.cwd(), process.argv[1], false, {is_ready: 'yes'}, cb);};
+    store(process.cwd(), process.argv[3], false, {is_ready: 'yes'}, cb);};
 
 module.exports = lambda_pattern;
---
+++ src/npm/gulpfile2.js
@@ -0,0 +1,49 @@
+const header = require('gulp-header');
+const footer = require('gulp-footer');
+const gulp = require('gulp');
+const replace = require('gulp-replace');
+const sequence = require('gulp-sequence')
+const babel = require('gulp-babel');
+const del = require('del');
+const debug = require('gulp-debug');
+const continuation = require('gulp-continuation');
+
+gulp.task('delete_src', ()=>{
+    return del(['src']);});
+
+gulp.task('copy_src', ()=>{
+    return gulp.src('../../src/**/*')
+            .pipe(gulp.dest('src'))});
+
+gulp.task('main_file', ()=>{
+    return gulp.src('src/[project_name]/[project_name].js')
+        .pipe(header('#!/usr/bin/env node\n\n'))
+        .pipe(footer(`
+if (!module.parent) {
+    lambda_pattern(function (er) {
+        if (er) {
+           console.log(er.toString());
+        }
+    });
+}`))
+        .pipe(gulp.dest('src/[project_name]'));});
+
+gulp.task('es6', ()=>{
+    return gulp.src('src/**/*.es6')
+        .pipe(replace(/\[project\_name\]/g, 'lambda_pattern'))
+        .pipe(replace(/cont\(.*err.*\).*;/g, '$& if (err) {return cb(err);};'))
+        .pipe(gulp.dest('src'))
+        .pipe(babel({ presets: ['es2015'] }))
+        .pipe(continuation())
+        .pipe(gulp.dest('src'));});
+
+gulp.task('copy_gulpfile_1', ()=>{
+    return gulp.src('src/npm/gulpfile.js')
+            .pipe(gulp.dest('../../src/npm'));});
+
+gulp.task('copy_gulpfile_2', ()=>{
+    return gulp.src('src/npm/gulpfile.js')
+            .pipe(gulp.dest('.'));});
+
+gulp.task('build_dev', sequence('copy_src', 'es6', 'main_file', 'copy_gulpfile_1', 'copy_gulpfile_2'));
+gulp.task('build_release',sequence('copy_src', 'es6'));
